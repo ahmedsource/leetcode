ce948bc6da1481a3821f51be04a910dc9f0646fd
Ahmed Baher <ahmedsource@gmail.com>
2024-10-26 20:47:07 +0300

    Time: 893 ms (28.36%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -2,5 +2,5 @@
 SELECT u.name AS NAME, SUM(t.amount) AS BALANCE
 FROM Users as u
 INNER JOIN Transactions AS t ON t.account = u.account
-GROUP BY u.name
-HAVING SUM(t.amount) > 10000
+GROUP BY NAME
+HAVING BALANCE > 10000
