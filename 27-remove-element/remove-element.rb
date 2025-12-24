8464fb21fc628a4027751aa82fedeec8cf505b66
Ahmed Baher <ahmedsource@gmail.com>
2025-12-24 21:17:49 +0200

    Time: 0 ms (100.00%) | Memory: 211.5 MB (52.91%) - LeetSync

--- 27-remove-element/remove-element.rb
+++ 27-remove-element/remove-element.rb
@@ -2,8 +2,15 @@
 # @param {Integer} val
 # @return {Integer}
 def remove_element(nums, val)
-  for i in 0...nums.length
-    nums.reject! {|x| x==val}
+  i = 0
+  l = nums.length
+  while i < l
+    if nums[i] == val
+      l-=1
+      nums[i] = nums[l]
+    else
+      i+=1
+    end
   end
-  nums.count
+  return l
 end
